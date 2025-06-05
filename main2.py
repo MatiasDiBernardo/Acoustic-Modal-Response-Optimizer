Fix FEM Source Spatial Average

main2.py
@@ -2,54 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-from FEM_source import FEM_Source_Solver, FEM_Source_Solver_Spatial_Average
+from FEM_source import FEM_Source_Solver, FEM_Source_Solver_Average
 
-# Solución sin adaptar grilla
-res = 2
+res = 1  # Resolución del salto de frecuencias
 freqs_eval = np.arange(20, 200, res)
-
 mesh = "mallado/room_max200.msh"  # Crear malla con el script correspondiente
-space_average = 6  # El agregar promediado espacial casi no afecta la preformance
+receptor_position1 = (2.8, 2.0, 1.8)
 
-receptor_position1 = (3.0, 2.0, 1.8)
-
+# Solución grilla común
 time_base = time.time()
-res_base = FEM_Source_Solver_Spatial_Average(freqs_eval, mesh, receptor_position1, space_average)
-res_base_prom = np.sum(res_base, axis=0)
+res_base = FEM_Source_Solver_Average(freqs_eval, mesh, receptor_position1)
+res_base_prom = np.sum(res_base, axis=0)/7
 t1 = time.time()
 time1 = t1 - time_base
 
 # Solución adaptando grilla
+res = 2
 f1 = np.arange(20, 80, res)
 mesh1 =  "mallado/room_max80.msh"
-res1 = FEM_Source_Solver_Spatial_Average(f1, mesh1, receptor_position1, space_average)
+res1 = FEM_Source_Solver_Average(f1, mesh1, receptor_position1)
 
 f2 = np.arange(80, 140, res)
 mesh2 =  "mallado/room_max140.msh"
-res2 = FEM_Source_Solver_Spatial_Average(f2, mesh2, receptor_position1, space_average)
+res2 = FEM_Source_Solver_Average(f2, mesh2, receptor_position1)
 
 f3 = np.arange(140, 200, res)
 mesh3 =  "mallado/room_max200.msh"
-res3 = FEM_Source_Solver_Spatial_Average(f3, mesh3, receptor_position1, space_average)
+res3 = FEM_Source_Solver_Average(f3, mesh3, receptor_position1)
 
 res_tot = np.hstack([res1, res2, res3])
-res_tot_prom = np.sum(res_tot, axis=0)
+res_tot_prom = np.sum(res_tot, axis=0) / 7
 f_tot =  np.hstack([f1, f2, f3])
 
 t2 = time.time()
 time2 = t2 - t1
 
+# Solución simple
+res_simple = FEM_Source_Solver(freqs_eval, mesh, receptor_position1)
+res_simple = 20 * np.log10(np.abs(res_simple))
+time3 = time.time() - t2
+
 print("El tiempo de cada ejecución fue: ")
-print("Tiempo normal: ", time1)
+print("Tiempo normal sin average: ", time3)
+print("Tiempo normal average: ", time1)
 print("Tiempo grilla adapt: ", time2)
 
-plt.plot(freqs_eval, res_base_prom,'x-', label="Base")
-plt.plot(f_tot, res_tot_prom, 'o-' ,label ="Adapt")
+plt.plot(freqs_eval, res_simple, "--", label="Base Central")
+plt.plot(freqs_eval, res_base_prom, "--", label="Average")
+plt.plot(f_tot, res_tot_prom, "--", label ="Average Grilla Adapt")
+# plt.plot(f_tot, res_base[0], "--")
+# plt.plot(f_tot, res_base[1], "--")
+# plt.plot(f_tot, res_base[2], "--")
+# plt.plot(f_tot, res_base[3], "--")
+# plt.plot(f_tot, res_base[4], "--")
+# plt.plot(f_tot, res_base[5], "--")
+# plt.plot(f_tot, res_base[6], "--")
 plt.legend()
-# plt.plot(freqs_eval, res1[1])
-# plt.plot(freqs_eval, res1[2])
-# plt.plot(freqs_eval, res1[3])
-# plt.plot(freqs_eval, res1[4])
-# plt.plot(freqs_eval, res1[5])
 plt.grid()
 plt.show()
+

mode_sumation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from FEM_source import FEM_Source_Solver_Spatial_Average, FEM_Source_Solver
+from FEM_source import FEM_Source_Solver_Average, FEM_Source_Solver
 from FEM_source_time import FEM_time
 
 def compute_modal_transfer(rs, rr, L, freqs, c=343.0, eta=0.01):
@@ -148,8 +148,8 @@ def compute_modal_transfer_complete(
 # Parametros
 L = (3.0, 4.0, 2.2)          # Room dimensions [m]
 rs = [1.0, 1.5, 1.2]        # Source position [m]
-rr = [3.0, 2.0, 1.8]        # Receiver position [m]
-freqs = np.arange(20, 200, 2)
+rr = [2.9, 2.0, 1.8]        # Receiver position [m]
+freqs = np.arange(20, 200, 1)
 mesh = "mallado/room_max200.msh"  # Crear malla con el script correspondiente
 
 # Modal Sum (resolución analítica sumando auto funciones)
@@ -157,36 +157,33 @@ rta = compute_modal_transfer(rs, rr, L, freqs)
 rta -= rta[0]
 
 # FEM Source común (Calculo por frecuencia de la ecuación de Hemholtz)
-time_start = time.time()
-res_fem = FEM_Source_Solver(freqs, mesh, rr)
-res_fem = 20 * np.log10(np.abs(res_fem))
-res_fem -= res_fem[0]
-time_end = time.time() - time_start
+# time_start = time.time()
+# res_fem = FEM_Source_Solver(freqs, mesh, rr)
+# res_fem = 20 * np.log10(np.abs(res_fem))
+# res_fem -= res_fem[0]
+# time_end = time.time() - time_start
 
 # FEM Source Promediado (calcula varios puntos en el espacio para dar la solución)
-#space_average = 6  # El agregar promediado espacial casi no afecta la preformance
-# res_base = FEM_Source_Solver_Spatial_Average(freqs, mesh, rr, space_average)
-# amp_dif = res_base[0][0] - rta[0]
-# rta += amp_dif
+# res_base = FEM_Source_Solver_Average(freqs, mesh, rr)
+# res_base_prom = np.sum(res_base, axis=0)/7
+# res_base_prom -= res_base_prom[0]
 
 # FEM Time (Simula en tiempo una respuesta al impulso y calcula RIR)
-# freqs_fem, res_fem = FEM_time(mesh, rr)
-# res_fem = 20 * np.log10(res_fem)
-# res_fem -= res_fem[0]
+freqs_fem, res_fem = FEM_time(mesh, rr)
+res_fem = 20 * np.log10(res_fem)
+res_fem -= res_fem[0]
 
-# Resultados guardades de simulación por Tiempo
-fem_time = np.load("mallado/mag_plot.npy")
-freqs_fem = np.load("mallado/freqs_fem.npy")
+# Resultados guardades de simulación por Tiempo (para comparar contra la otra versión)
+# fem_time = np.load("mallado/mag_plot.npy")
+# freqs_fem = np.load("mallado/freqs_fem.npy")
 
 plt.figure()
-plt.plot(freqs, rta, label="Modal Summation")
-#plt.plot(freqs, res_base[0], label="FEM Promediado")
-plt.plot(freqs, res_fem, label="FEM Source")
-plt.plot(freqs_fem, fem_time, label="FEM Time")
+plt.plot(freqs, rta, '--', label="Modal Summation", linewidth=2)
+# plt.plot(freqs, res_base_prom, '--', label="FEM Promediado", linewidth=2)
+# plt.plot(freqs, res_fem, label="FEM Source")
+plt.plot(freqs_fem, res_fem, '--', label="FEM Time", linewidth=2)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-print("El tiempo que tardo en procesar fue: ", time_end)